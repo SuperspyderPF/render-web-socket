c78070f27b00d4f572189c2b1dc147dd0325bb78
unknown <djasherov@gmail.com>
2024-07-04 13:13:16 -0700

    websocket

---
+++ renderServer.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let clients = [];
+
+wss.on('connection', (ws) => {
+  clients.push(ws);
+
+  ws.on('message', (message) => {
+    clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString()); 
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients = clients.filter((client) => client !== ws);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
